4aacec7bfad038fa4944f0a7c423f53986084e63
Yosuke Higuchi <wasapon.dev@gmail.com>
2017-12-22 13:49:45 +0900

    update

--- IDS.cpp
+++ IDS.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
+#include <time.h>
+
 #include "state.hpp"
 
 using namespace std;
 
 const string goal = "123456789abcdef-";
 
-void ids(int limit);
+bool ids();
 
 int main() {
-    state_show(goal);
+    clock_t tic, toc;
+
+    int shuffle;
+    cout << "Input shuffle counts >> ";
+    cin >> shuffle;
+
+    State first = state_get_random(goal, shuffle);
 }
 
+bool ids() {
+    
+}
+
+void show_time(clock_t t) {
+    printf("%-8s%f [s]\n", "time:", (double)(t) / CLOCKS_PER_SEC);
+}
