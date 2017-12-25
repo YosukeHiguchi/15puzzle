7f086af78c62819919acfa9650b8ad1643ec29d7
Yosuke Higuchi <wasapon.dev@gmail.com>
2017-12-25 16:18:03 +0900

    update

---
+++ js/global.js
@@ -0,0 +1,17 @@
+function globalFunc() {
+    ctx = undefined;
+    W = 400;
+    H = 400;
+    img = [];
+    state_idx = {
+        "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "a": 10,
+        "b": 11, "c": 12, "d": 13, "e": 14, "f": 15, "-": 16
+    }
+
+    main_state = undefined;
+
+    N = 4;
+    goal = "123456789abcdef-";
+
+    key_lock = false;
+}
